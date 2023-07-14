7fe29ce459dcd7f84d5e69889ab0de0bb8afba4e
mariamolgeorge123 <mariadaisy1990@gmail.com>
2023-07-13 23:12:58 -0400

    save date

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,8 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("hai");
+        System.out.println("haijhhas");
+        System.out.println("haiasmnas");
+        System.out.println("haihahskajksjaslk");
     }
 }
