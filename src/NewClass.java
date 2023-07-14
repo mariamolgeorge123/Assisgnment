c31e8ffe2089eb4eaadebf11c68e86c756bafc06
mariamolgeorge123 <mariadaisy1990@gmail.com>
2023-07-13 23:31:39 -0400

    multiplication

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,8 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("hai");
+        int x=10;
+        int y=11;
+        System.out.println(x*y);
     }
 }
